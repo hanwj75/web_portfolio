282e16bc21bde908bda9dc5353bd9ba503dcf7af
hanwj75 <wjws9608@gmail.com>
2025-04-25 22:28:49 +0900

    Update : "mysql 스키마 추가"
    users , portfolios , sections 스키마 작성

---
+++ src/db/sql/sections.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `sections` (
+  `id` CHAR(36) PRIMARY KEY,
+  `portfolioId` CHAR(36) NOT NULL,
+  `type` ENUM('home','uxui','webRedesign','detailpage') NOT NULL,
+  `content` JSON,
+  `sortOrder` INT NOT NULL,
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (portfolioId) REFERENCES portfolios(id) ON DELETE CASCADE
+  )
