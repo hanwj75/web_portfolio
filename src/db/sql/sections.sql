95ef47f2532b19322ed94958c84ff64fd52f5e2c
hanwj75 <wjws9608@gmail.com>
2025-04-26 04:25:15 +0900

    Update : "마이그레이션 작업 완료"

--- src/db/sql/sections.sql
+++ src/db/sql/sections.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `sections` (
+CREATE TABLE IF NOT EXISTS `Sections` (
   `id` CHAR(36) PRIMARY KEY,
   `portfolioId` CHAR(36) NOT NULL,
   `type` ENUM('home','uxui','webRedesign','detailpage') NOT NULL,
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS `sections` (
   `sortOrder` INT NOT NULL,
   `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (portfolioId) REFERENCES portfolios(id) ON DELETE CASCADE
+  FOREIGN KEY (portfolioId) REFERENCES Portfolios(id) ON DELETE CASCADE
   )
