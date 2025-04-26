5e5f0ea81d86c326fa786d73e8d1c861cc40cc52
hanwj75 <wjws9608@gmail.com>
2025-04-27 05:11:22 +0900

    Update : "portfolio 쿼리모듈 추가"

--- src/db/sql.queries.js
+++ src/db/sql.queries.js
@@ -6,12 +6,13 @@ export const SQL_QUERIES = {
   CREATE_USER: `INSERT INTO Users (id,email, password, userName) VALUES (?,?,?,?)`,
   UPDATE_USER_LOGIN: `UPDATE Users SET updatedAt = CURRENT_TIMESTAMP WHERE email = ?`,
   DELETE_USER: `DELETE FROM Users WHERE id = ?`,
-
   UPDATE_USER_NAME: `UPDATE Users SET userName = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
   UPDATE_USER_PASSWORD: `UPDATE Users SET password = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
   UPDATE_USER_ALL: `UPDATE Users SET userName = ?, password = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
 
   //Portfolios
-
+  CREATE_PORTFOLIO: `INSERT INTO Portfolios (id,userId,title,isPublic,publicUrlId) VALUES(?,?,?,?,?)`,
+  FIND_PORTFOLIO_BY_UUID: `SELECT * FROM Portfolios WHERE id = ?`,
+  DELETE_PORTFOLIO: `DELETE FROM Portfolios WHERE id = ?`,
   //Sections
 };
