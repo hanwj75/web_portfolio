82b7274f35205d014a1be8392a95663ce8fc65b1
hanwj75 <wjws9608@gmail.com>
2025-04-27 05:29:03 +0900

    Add : "섹션 라우터 생성"

---
+++ src/routes/sections.router.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router();
+
+/**
+ * @desc 섹션 추가 API
+ */
+
+/**
+ * @desc 섹션 내용 수정 API
+ */
+
+/**
+ * @desc 섹션 순서 일괄 변경 API
+ */
+
+/**
+ * @desc 섹션 삭제 API
+ */
+
+export default router;
