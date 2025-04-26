df2fa0c72a720c1d2fc2bb14ece8090435683fe3
hanwj75 <wjws9608@gmail.com>
2025-04-26 23:35:48 +0900

    Update : "유저정보 분할 업데이트 쿼리 추가"

--- src/db/sql.queries.js
+++ src/db/sql.queries.js
@@ -6,7 +6,10 @@ export const SQL_QUERIES = {
   CREATE_USER: `INSERT INTO Users (id,email, password, userName) VALUES (?,?,?,?)`,
   UPDATE_USER_LOGIN: `UPDATE Users SET updatedAt = CURRENT_TIMESTAMP WHERE email = ?`,
   DELETE_USER: `DELETE FROM Users WHERE email = ?`,
-  UPDATE_USER_DATA: `UPDATE Users SET userName = ?, email = ?, updatedAt = CURRENT_TIMESTAMP  WHERE id = ?`,
+
+  UPDATE_USER_NAME: `UPDATE Users SET userName = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
+  UPDATE_USER_PASSWORD: `UPDATE Users SET password = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
+  UPDATE_USER_ALL: `UPDATE Users SET userName = ?, password = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
 
   //Portfolios
 
