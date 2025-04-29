98db95bdf182effff1183948ecf485a9db7aa9b7
hanwj75 <wjws9608@gmail.com>
2025-04-29 21:15:59 +0900

    Update : "cors , swagger 연결"

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import config from "./config/config.js";
 import initServer from "./init/index.js";
 import usersRouter from "./routes/users.router.js";
 import portfoliosRouter from "./routes/portfolios.router.js";
+import { specs, swaggerUi } from "./config/swagger.js";
 const app = express();
 const { server } = config;
 
-// app.use(
-//   cors({
-//     origin: ["http://43.203.119.163:3333", "http://localhost:3333"],
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-//   }),
-// );
+app.use(
+  cors({
+    origin: ["http://34.59.2.83:3333", "http://localhost:3333", "http://127.0.0.1:3333"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  }),
+);
 
 app.use(express.json());
 
 // swagger 문서 확인 페이지
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // api 라우터 설정
 app.use("/api", [usersRouter, portfoliosRouter]);
