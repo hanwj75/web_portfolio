3a79178eae1a6bfdd3f2607d9096d2b2a0a0bb3b
hanwj75 <wjws9608@gmail.com>
2025-04-29 16:27:16 +0900

    Update : "포트폴리오 db 상세조회 함수 추가"

--- src/db/portfolio/portfolios.db.js
+++ src/db/portfolio/portfolios.db.js
@@ -34,12 +34,53 @@ export const findUserPortfolios = async (userId) => {
   }
 };
 
+export const findPortfolioWithSections = async (portfoliosId, userId) => {
+  try {
+    const [rows] = await pools.PORTFOLIOS_DB.query(SQL_QUERIES.FIND_PORTFOLIO_WITH_SECTIONS, [
+      portfoliosId,
+      userId,
+    ]);
+
+    if (!rows || rows.length === 0) return null;
+
+    // 포트폴리오 기본 정보
+    const portfolio = {
+      portfolioId: rows[0].id,
+      title: rows[0].title,
+      createdAt: rows[0].createdAt,
+      updatedAt: rows[0].updatedAt,
+      sections: [],
+    };
+    //섹션 상세 정보
+    rows.forEach((row) => {
+      if (row.sectionId) {
+        let content = row.content;
+        if (typeof content === "string") {
+          try {
+            content = JSON.parse(content);
+          } catch (err) {}
+        }
+        portfolio.sections.push({
+          id: row.sectionId,
+          type: row.type,
+          content,
+          sortOrder: row.sortOrder,
+        });
+      }
+    });
+
+    return portfolio;
+  } catch (err) {
+    console.error(`포트폴리오 상세 조회 에러${err}`, err);
+  }
+};
+
 export const findPortfolioByUUID = async (id) => {
   try {
     const [rows] = await pools.PORTFOLIOS_DB.query(SQL_QUERIES.FIND_PORTFOLIO_BY_UUID, [id]);
     return toCamelCase(rows[0]);
   } catch (err) {
-    console.error(`포트폴리오 조회 에러${err}`, err);
+    console.error(`포트폴리오ID 조회 에러${err}`, err);
   }
 };
 
