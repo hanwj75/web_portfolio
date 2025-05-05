d49514ec23bdcc3d38a317bb326e156593514c27
hanwj75 <wjws9608@gmail.com>
2025-05-05 21:11:32 +0900

    Update : "포트폴리오 관련 쿼리 작성"
    포트폴리오 공개상태 수정 쿼리 , 포트폴리오 전체 데이터 조회 쿼리 추가

--- src/db/sql.queries.js
+++ src/db/sql.queries.js
@@ -28,13 +28,17 @@ export const SQL_QUERIES = {
   `,
   //포트폴리오 삭제
   DELETE_PORTFOLIO: `DELETE FROM Portfolios WHERE id = ?`,
-  //포트폴리오 배포 상태 업데이트
-  DEPLOY_PORTFOLIO: `UPDATE Portfolios SET isPublic = true, publicUrlId = ?,updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
+  //포트폴리오 배포 상태 업데이트(공개)
+  DEPLOY_PORTFOLIO: `UPDATE Portfolios SET isPublic = true, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`,
+  //포트폴리오 배포 상태 업데이트(비공개)
+  UNDEPLOY_PORTFOLIO: `UPDATE Portfolios SET isPublic = false WHERE id = ? AND userId = ?`,
   // publicUrlId로 포트폴리오와 섹션 조회
-  FIND_PORTFOLIO_BY_PUBLIC_ID: `SELECT p.*, s.id as sectionId, s.type, s.content, s.sortOrder FROM Portfolios p LEFT JOIN Sections s ON p.id = s.portfolioId 
-WHERE p.publicUrlId = ? AND p.isPublic = true 
-ORDER BY s.sortOrder ASC
-`,
+  FIND_PORTFOLIO_BY_PUBLIC_ID: `SELECT p.*, c.id as categoryId, c.name as categoryName, c.type as categoryType, s.id as sectionId, s.content
+    FROM Portfolios p 
+    LEFT JOIN Categories c ON p.id = c.portfolioId
+    LEFT JOIN Sections s ON c.id = s.categoryId
+    WHERE p.publicUrlId = ? AND p.isPublic = true 
+    ORDER BY c.sortOrder ASC`,
 
   //Sections
   //카테고리별 섹션 조회
