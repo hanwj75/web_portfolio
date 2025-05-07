678bc302804d163414a08959e3e5d383eb8e1c25
hanwj75 <wjws9608@gmail.com>
2025-05-07 13:32:35 +0900

    Update : "카테고리 순서 재정령 API 추가"

--- src/routes/categories.router.js
+++ src/routes/categories.router.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { jwtMiddleware } from "../middlewares/auth.middleware.js";
-import { createCategory, findCategoriesByPortfolio } from "../db/category/categories.db.js";
+import {
+  createCategory,
+  findCategoriesByPortfolio,
+  reorderCategories,
+} from "../db/category/categories.db.js";
 
 const router = express.Router();
 
@@ -85,6 +89,49 @@ router.get("/portfolios/categories", async (req, res, next) => {
   } catch (err) {
     console.error(`카테고리 조회 에러${err}`, err);
     return res.status(500).json({ message: "카테고리 조회 실패" });
+  }
+});
+
+/**
+ * @desc 카테고리 순서 재정렬
+ * @header x-portfolio-id: 포트폴리오 UUID
+ * @body
+ * {firstCategoryId: "카테고리 1번", "secondCategoryId":"카테고리4번"}
+ */
+
+router.patch("/categories/reorder", jwtMiddleware, async (req, res, next) => {
+  try {
+    const portfolioId = req.headers["x-portfolio-id"];
+    const { firstCategoryId, secondCategoryId } = req.body;
+
+    if (!portfolioId || !firstCategoryId || !secondCategoryId) {
+      return res.status(400).json({ message: "필수값 누락" });
+    }
+    //카테고리 조회
+    const category = await findCategoriesByPortfolio(portfolioId);
+    const firstCategory = category.find((c) => c.id === firstCategoryId);
+    const secondCategory = category.find((c) => c.id === secondCategoryId);
+
+    if (!firstCategory || !secondCategory) {
+      return res.status(404).json({ message: "카테고리 조회 실패" });
+    }
+
+    //순서 교환
+    const result = await reorderCategories(
+      portfolioId,
+      firstCategoryId,
+      firstCategory.sortOrder,
+      secondCategoryId,
+      secondCategory.sortOrder,
+    );
+
+    return res.status(200).json({
+      message: "카테고리 순서 재정렬 완료",
+      data: result,
+    });
+  } catch (err) {
+    console.error(`카테고리 순서 재정렬 에러${err}`, err);
+    return res.status(500).json({ message: "카테고리 순서 재정렬 실패" });
   }
 });
 
