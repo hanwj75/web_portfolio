ddd8595c99691fa8141e9d2b643aa992b7ff4378
hanwj75 <wjws9608@gmail.com>
2025-05-13 10:44:22 +0900

    Add : "구글 passport 설정파일 추가"

---
+++ src/passport/google.js
@@ -0,0 +1,34 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { findUserByEmail, createUser, findUserByUUID } from "../db/user/user.db.js";
+import config from "../config/config.js";
+
+const dotGoogle = config.google;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: dotGoogle.GOOGLE_CLIENT_ID,
+      clientSecret: dotGoogle.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      //profile에서 이메일, 이름 등 추출
+      const email = profile.emails[0].value;
+      let user = await findUserByEmail(email);
+      if (!user) {
+        user = await createUser(email, "SOCIAL", profile.displayName);
+      }
+      return done(null, user);
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await findUserByUUID(id);
+
+  done(null, id);
+});
