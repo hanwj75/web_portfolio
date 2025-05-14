9d94f9ab35535fc792cc96a30c7be9eff6991ddd
hanwj75 <wjws9608@gmail.com>
2025-05-14 22:00:54 +0900

    Update : "네이버 환경변수 추가"

--- src/config/config.js
+++ src/config/config.js
@@ -16,6 +16,8 @@ const config = {
     SESSION_SECRET_KEY: envData.social.SESSION_SECRET_KEY,
     KAKAO_CLIENT_ID: envData.social.KAKAO_CLIENT_ID,
     KAKAO_CLIENT_SECRET: envData.social.KAKAO_CLIENT_SECRET,
+    NAVER_CLIENT_ID: envData.social.NAVER_CLIENT_ID,
+    NAVER_CLIENT_SECRET: envData.social.NAVER_CLIENT_SECRET,
   },
 };
 
