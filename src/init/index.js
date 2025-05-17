66219b41808c77af10b56acc8e1fec23d4d8037b
hanwj75 <wjws9608@gmail.com>
2025-05-17 20:21:26 +0900

    Update : "서버 실행 사전작업 redis 추가"

--- src/init/index.js
+++ src/init/index.js
@@ -1,10 +1,13 @@
 import pools from "../db/database.js";
+import { connectRedis } from "../db/redis/redis.js";
+
 import { testAllConnections } from "../utils/db/testConnection.js";
 
 //서버 실행전 사전작업
 const initServer = async () => {
   try {
     await testAllConnections(pools);
+    await connectRedis();
   } catch (err) {
     console.error(`Server Init Error : ${err}`);
     process.exit(1);
