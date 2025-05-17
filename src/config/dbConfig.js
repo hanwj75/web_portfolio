6db45f2594841e968c9af2f10f53554a0fbd1385
hanwj75 <wjws9608@gmail.com>
2025-05-17 19:37:09 +0900

    Update : "레디스 환경변수 추가"

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,7 @@
 import envData from "../constants/env.js";
 
 const dotDb = envData.database;
+const dotRedis = envData.redis;
 
 const dbConfig = {
   database: {
@@ -11,6 +12,11 @@ const dbConfig = {
       host: dotDb.DB1_HOST,
       port: dotDb.DB1_PORT,
     },
+  },
+  redis: {
+    REDIS_HOST: dotRedis.REDIS_HOST,
+    REDIS_PORT: dotRedis.REDIS_PORT,
+    REDIS_PASSWORD: dotRedis.REDIS_PASSWORD,
   },
 };
 
